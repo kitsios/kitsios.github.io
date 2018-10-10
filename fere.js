0fa002132b91d8eab35e0ea594e8857136f82e59
kitsios <44033643+kitsios@users.noreply.github.com>
2018-10-10 19:50:34 +0300

    First website upload

---
+++ fere.js
@@ -0,0 +1,190 @@
+var img;
+function preload() {
+  img = loadImage('car.jpg');
+    imgc = loadImage('carl.png');
+  imgk = loadImage('keo.png');
+imgb = loadImage('back.png');
+  
+}
+
+
+function setup() {
+  createCanvas(500, 500);
+  xx = width/1.25;
+	yy = height/3;
+	ww = 15;
+	hh = 50;
+	x = width/2;
+	y = height/2;
+	w = 15;
+	h = 50;
+  ex = 300;
+  ey = 100;
+  ew = 100;
+  eh = 50;
+  lives = 3;
+  score = 0;
+  bon = 0;
+}
+ 
+
+function draw() {
+  background(imgb);
+ 
+ 
+  image(img,ex,ey,ew,eh);
+ ////fill(255,0,0);
+///rect(ex,ey,ew,eh);
+  ex = mouseX;
+  ey = mouseY;
+	//fill(0,50,0);
+	image(imgc,x,y,w,h);
+ // fill(50,0,50);
+  image(imgk,xx,yy,ww,hh);
+  textSize(25);
+  fill(255,0,0);
+  text('lives:',30,30);
+  fill(255,0,0);
+  text(lives,100,30);
+   fill(0,0,255);
+  text('score:',30,70);
+  fill(0,0,255);
+  text(score,100,70);
+   fill(255);
+  line(0,10,30,500);
+  if(x>0){x-=8;}
+  else{x = width;
+       y = random(50,450);
+      }
+  if (x < ex + ew &&
+  x + w > ex &&
+  y < ey + eh &&
+  h + y > ey){
+  x = width;
+  y = random(50,450);
+  lives -=1;}
+  if(lives <= 0){background (255,0,0);
+                 
+                 text('GAME OVER',180,250);
+				 text(score,260,280);}
+    
+  
+  if(xx>0){xx-=8;}
+  else{xx = width;
+       yy = random(50,450);
+      }
+  if (xx < ex + ew &&
+  xx + ww > ex &&
+  yy < ey + eh &&
+  hh + yy > ey){
+  xx = width;
+  yy = random(50,450);
+  score += 1;
+  bon +=1;
+  }
+  if(score >=10)
+  { x -= 1;
+	xx -= 1;
+	}
+if(score >=20)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=30)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=40)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=50)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=60)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=70)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=80)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=90)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=100)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=110)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=120)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=130)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=140)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=150)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=160)
+  { x -= 1;
+	xx -= 1;
+	}
+	if(score >=170)
+  { x -= 1;
+	xx -= 1;
+	}
+	
+ if(score == 24 && lives <= 2){
+	 
+	 lives += 1;
+	 
+ }
+ if(score == 48 && lives <= 2){
+	 
+	 lives += 1;
+	 
+ }
+ if(score == 72 && lives <= 2){
+	 
+	 lives += 1;
+	 
+ }
+ if(score == 96 && lives <= 2){
+	 
+	 lives += 1;
+	 
+ }
+ if(score == 120 && lives <= 2){
+	 
+	 lives += 1;
+	 
+ }
+ if(score == 144 && lives <= 2){
+	 
+	 lives += 1;
+	 
+ }
+ if(score == 168 && lives <= 2){
+	 
+	 lives += 1;
+	 
+ }
+	
+}
